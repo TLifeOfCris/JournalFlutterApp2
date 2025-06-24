51792362bc604c9eccf0e36f7f0acaeca3c3acc5
TLifeOfCris <crissandoval150201@gmail.com>
2025-06-24 12:19:24 -0600

    Se le agregó el model en la carpeta models

---
+++ flutter_journal_app/lib/models/journy.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Journey {
+  //ID de la nota
+  final String id;
+  //Contenido de la nota
+  final String content;
+  //Mood: Excelente, Feliz, MEH, aburrido, malo
+  final String mood;
+  //Tiempo en que se creó la nota
+  final DateTime timestamp; 
+
+  Journey({ 
+  required this.id, 
+  required this.content, 
+  required this.mood, 
+  required this.timestamp});
+
+}
