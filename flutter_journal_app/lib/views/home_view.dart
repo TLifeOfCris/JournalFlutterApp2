4f64b4eab3309d028857f356e876f99f15d9a1cf
TLifeOfCris <crissandoval150201@gmail.com>
2025-07-07 15:18:09 -0600

    ya se pueden eliminar las tarjetas de Entrie

--- flutter_journal_app/lib/views/home_view.dart
+++ flutter_journal_app/lib/views/home_view.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_journal_app/models/journy.dart';
 import 'package:flutter_journal_app/viewmodels/providerviewmodel.dart';
 import 'package:flutter_journal_app/widgets/entry.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
-
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -16,13 +16,30 @@ class HomeView extends StatelessWidget {
     return Scaffold(
 
       body: ListView.builder(
-        itemCount: entries.length,
-        itemBuilder: (context, index){
-          final journey = entries[index];
-        
-        return EntryCard(list: journey);
-      })
+          itemCount: entries.length,
+          itemBuilder: (context, index){
+            final journey = entries[index];
+          
+          return Slidable(
+            key: ValueKey(journey.id),
+            //revisar 
+            endActionPane: ActionPane(motion: ScrollMotion(), children: [
+              SlidableAction(onPressed: (_){
+                JournalVM.deleteEntrie(entries.length - 1 - index);
+                
+              },
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'borrar'
+              )
+            ]),
+            
+            
+            child:  EntryCard(list: journey));
+        }),
+      )
       
-    );
+    ;
   }
 }
--- flutter_journal_app/lib/widgets/entry.dart
+++ flutter_journal_app/lib/widgets/entry.dart
@@ -32,56 +32,59 @@ class EntryCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
-      elevation: 3,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          //Barra superior
-          Container(width: double.infinity,
-          //personalizar dependiendo del mood
-          color: Color.fromARGB(200, 119, 221, 119),
-          padding: EdgeInsets.all(8),
-          //personalizar
-          //TODAY, YESTERDAY, SUNDAY,ETC
-          //child: Text("Viernes 13"),
-          
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(Icons.adjust),
-              SizedBox(width: 8.0,),
-              Text(getFormattedDate(list.timestamp)),
-            ],
-          ),
-          
-          ),
-          Padding(padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //fila con carita mood y hora
-              Row(
-                children: [
-                  //en el text tiene que ir el mood
-                  //Text("Feliz", style: TextStyle(fontSize: 20),),
-                  Icon(list.mood, color: getMoodColor(list.mood),),
-                  SizedBox(width: 8,),
-                  Text(getMoodLabel(list.mood)),
-                  SizedBox(width: 8.0,),
-                  //content
-                  //Text("Hola"),
-                  Text(list.content),
-                  Spacer(),
-                  //Hora en la que se creó
-                  //Text("12:00")
-                  Text(  DateFormat('jm').format(list.timestamp)),
-                ],
-              )
-            ],
-          ),)
-        ],
+    return SizedBox(
+      width: double.infinity,
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+        elevation: 3,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //Barra superior
+            Container(width: double.infinity,
+            //personalizar dependiendo del mood
+            color: Color.fromARGB(200, 119, 221, 119),
+            padding: EdgeInsets.all(8),
+            //personalizar
+            //TODAY, YESTERDAY, SUNDAY,ETC
+            //child: Text("Viernes 13"),
+            
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.adjust),
+                SizedBox(width: 8.0,),
+                Text(getFormattedDate(list.timestamp)),
+              ],
+            ),
+            
+            ),
+            Padding(padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //fila con carita mood y hora
+                Row(
+                  children: [
+                    //en el text tiene que ir el mood
+                    //Text("Feliz", style: TextStyle(fontSize: 20),),
+                    Icon(list.mood, color: getMoodColor(list.mood),),
+                    SizedBox(width: 8,),
+                    Text(getMoodLabel(list.mood)),
+                    SizedBox(width: 8.0,),
+                    //content
+                    //Text("Hola"),
+                    Text(list.content),
+                    Spacer(),
+                    //Hora en la que se creó
+                    //Text("12:00")
+                    Text(  DateFormat('jm').format(list.timestamp)),
+                  ],
+                )
+              ],
+            ),)
+          ],
+        ),
       ),
     );
   }
