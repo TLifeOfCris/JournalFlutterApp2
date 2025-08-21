a9157706531df9fc97bd56828a247050a9291222
TLifeOfCris <crissandoval150201@gmail.com>
2025-08-21 12:05:01 -0600

    Se agregó confirguración de firebase en main

--- flutter_journal_app/lib/main.dart
+++ flutter_journal_app/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_journal_app/firebase_options.dart';
 import 'package:flutter_journal_app/viewmodels/providerviewmodel.dart';
 import 'package:flutter_journal_app/views/home_view.dart';
 import 'package:flutter_journal_app/views/add_entry_view.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  //Asegura que todo este listo antes de iniciar firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
